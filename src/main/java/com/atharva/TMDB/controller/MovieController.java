b179fc9ffceec2520e84a34af907085a787a08e3
AtharvaKakade <kakadeatharva5@gmail.com>
2025-04-03 10:40:13 +0530

    feat(account): add ACCOUNT module

--- src/main/java/com/atharva/TMDB/controller/MovieController.java
+++ src/main/java/com/atharva/TMDB/controller/MovieController.java
@@ -17,6 +17,92 @@ public class MovieController {
         this.tmdbService = tmdbService;
     }
 
+    //Section-ACCOUNT
+    //Get the public details of an account on TMDB.
+    @GetMapping("/account/{accountId}")
+    public Mono<String> getAccountDetails(@PathVariable String accountId) {
+        return tmdbService.getAccountDetails(accountId);
+    }
+
+    //Mark a movie or TV show as a favourite.
+    @PostMapping("/account/{accountId}/favorite")
+    public Mono<String> addFavorite(
+            @PathVariable String accountId,
+            @RequestBody Map<String, Object> request) {
+
+        int mediaId = (int) request.get("media_id");
+        String mediaType = (String) request.get("media_type");
+        boolean favorite = (boolean) request.get("favorite");
+
+        return tmdbService.addFavorite(accountId, mediaId, mediaType, favorite);
+    }
+
+
+
+    //Add a movie or TV show to your watchlist.
+    @PostMapping("/account/{accountId}/watchlist")
+    public Mono<String> addToWatchlist(
+            @PathVariable String accountId,
+            @RequestBody Map<String, Object> request){
+        int mediaId = (int)request.get("media_id");
+        String mediaType = (String)request.get("media_type");
+        boolean watchlist = (boolean)request.get("watchlist");
+        return tmdbService.addToWatchList(accountId,mediaId,mediaType,watchlist);
+    }
+
+    //Get a users list of favourite movies.
+    @GetMapping("/account/{accountId}/favorite/movies")
+    public  Mono<String> getFavMovies(@PathVariable String accountId){
+        return tmdbService.getFavoriteMovies(accountId);
+    }
+
+    //Get a users list of favourite TV shows.
+    @GetMapping("/account/{accountId}/favorite/tv")
+    public  Mono<String> getFavTv(@PathVariable String accountId){
+        return tmdbService.getFavoriteTv(accountId);
+    }
+
+    //Get a users list of custom lists.
+    @GetMapping("account/{accountId}/lists")
+    public Mono<String> getUserList(String accountId){
+        return tmdbService.getUserList(accountId);
+    }
+
+    //Get a users list of rated movies.
+    @GetMapping("/account/{accountId}/rated/movies")
+    public Mono<String> getUsersListOfRatedMovies(String accountId){
+        return tmdbService.getUsersListOfRatedMovies(accountId);
+    }
+
+    //Get a users list of rated TV shows.
+    @GetMapping("/account/{accountId}/rated/tv")
+    public Mono<String> getUsersListOfRatedTv(String accountId){
+        return tmdbService.getUsersListOfRatedTv(accountId);
+    }
+
+    //Get a users list of rated TV episodes.
+    @GetMapping("account/{accountId}/rated/tv/episodes")
+    public Mono<String> getListOfUserRatedTvEpisodes(String accountId){
+        return tmdbService.getListOfUserRatedTvEpisodes(accountId);
+    }
+
+    //Get a list of movies added to a users watchlist.
+    @GetMapping("account/{accountId}/watchlist/movies")
+    public Mono<String> getListOfMoviesAddedToUsersWatchlist(String accountId){
+        return tmdbService.getListOfMoviesAddedToUsersWatchlist(accountId);
+    }
+
+    //Get a list of TV shows added to a users watchlist.
+    @GetMapping("account/{accountId}/watchlist/tv")
+    public Mono<String> getListOfTvAddedToUsersWatchlist(String accountId){
+        return tmdbService.getListOfTvAddedToUsersWatchlist(accountId);
+    }
+    //SECTION-ACCOUNT-END
+
+
+
+
+
 
 
     @GetMapping("/discover/movies")
@@ -50,26 +136,14 @@ public class MovieController {
         return tmdbService.getNowPlayingMovies();
     }
 
-    @GetMapping("/account/{accountId}")
-    public Mono<String> getAccountDetails(@PathVariable String accountId) {
-        return tmdbService.getAccountDetails(accountId);
-    }
-
     @GetMapping("/movie/{movieId}")
-    public  Mono<String> getAccountById(@PathVariable String movieId){
+    public  Mono<String> getMovieById(@PathVariable String movieId){
         return tmdbService.getMovieByID(movieId);
     }
 
-    @PostMapping("/account/{accountId}/favorite")
-    public Mono<String> markAsFavorite(
-            @PathVariable String accountId,
-            @RequestBody Map<String, Object> request) {
-
-        int mediaId = (int) request.get("media_id");
-        String mediaType = (String) request.get("media_type");
-        boolean favorite = (boolean) request.get("favorite");
-
-        return tmdbService.markAsFavorite(accountId, mediaId, mediaType, favorite);
+    @GetMapping("watch/providers/regions")
+    public Mono<String> getAvailableRegions(){
+        return tmdbService.getAvailableRegions();
     }
 
 }
--- src/main/java/com/atharva/TMDB/service/TmdbService.java
+++ src/main/java/com/atharva/TMDB/service/TmdbService.java
@@ -2,13 +2,11 @@ package com.atharva.TMDB.service;
 
 import com.atharva.TMDB.model.MovieResponse;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
-import reactor.util.retry.Retry;
 
-import java.time.Duration;
+import javax.xml.transform.sax.SAXResult;
 import java.util.Map;
 
 @Service
@@ -26,6 +24,164 @@ public class TmdbService {
     public TmdbService(WebClient webClient) {
         this.webClient = webClient;
     }
+
+    //Section-ACCOUNT
+    //Get the public details of an account on TMDB.
+    public Mono<String> getAccountDetails(String accountId) {
+        return webClient.get()
+                .uri("/account/" + accountId)
+                .headers(headers -> {
+                    headers.setBearerAuth(apiToken);  // Set Bearer token dynamically
+                    headers.set("Accept", "application/json");
+                })
+                .retrieve()
+                .bodyToMono(String.class);
+    }
+
+    //Mark a movie or TV show as a favourite.
+    public Mono<String> addFavorite(String accountId, int mediaId, String mediaType, boolean favorite) {
+        String url = "/account/" + accountId + "/favorite";
+
+        Map<String, Object> requestBody = Map.of(
+                "media_type", mediaType,  // "movie" or "tv"
+                "media_id", mediaId,
+                "favorite", favorite
+        );
+
+        return webClient.post()
+                .uri(url)
+                .headers(headers -> {
+                    headers.setBearerAuth(apiToken);  // Authentication
+                    headers.set("Accept", "application/json");
+                    headers.set("Content-Type", "application/json");
+                })
+                .bodyValue(requestBody)  // Set JSON request body
+                .retrieve()
+                .bodyToMono(String.class);
+    }
+
+    public Mono<String> addToWatchList(String accountId, int mediaId, String mediaType, boolean watchlist) {
+        String url = "/account/" + accountId + "/watchlist";
+
+        Map<String, Object> requestBody = Map.of(
+                "media_type", mediaType,  // "movie" or "tv"
+                "media_id", mediaId,
+                "watchlist", watchlist
+        );
+
+        return webClient.post()
+                .uri(url)
+                .headers(headers -> {
+                    headers.setBearerAuth(apiToken);  // Authentication
+                    headers.set("Accept", "application/json");
+                    headers.set("Content-Type", "application/json");
+                })
+                .bodyValue(requestBody)  // Set JSON request body
+                .retrieve()
+                .bodyToMono(String.class);
+    }
+
+    //Get a users list of favourite movies.
+    public Mono<String> getFavoriteMovies(String accountId){
+        return webClient.get()
+                .uri("/account/"+accountId+"/favorite/movies")
+                .headers(headers -> {
+                    headers.setBearerAuth(apiToken);  // Set Bearer token dynamically
+                    headers.set("Accept", "application/json");
+                })
+                .retrieve()
+                .bodyToMono(String.class);
+    }
+
+    //Get a users list of favourite TV shows.
+    public Mono<String> getFavoriteTv(String accountId){
+        return webClient.get()
+                .uri("/account/"+accountId+"/favorite/tv")
+                .headers(headers -> {
+                    headers.setBearerAuth(apiToken);  // Set Bearer token dynamically
+                    headers.set("Accept", "application/json");
+                })
+                .retrieve()
+                .bodyToMono(String.class);
+    }
+
+    //Get a users list of custom lists.
+    public Mono<String> getUserList(String accountId){
+        return webClient.get()
+                .uri("/account/"+accountId+"/lists")
+                .headers(headers->{
+                    headers.setBearerAuth(apiToken);
+                    headers.set("Accept", "application/json");
+                })
+                .retrieve()
+                .bodyToMono(String.class);
+    }
+
+    //Get a users list of rated movies.
+    public Mono<String> getUsersListOfRatedMovies(String accountId){
+        return webClient.get()
+                .uri("/account/"+accountId+"/rated/movies")
+                .headers(headers->{
+                    headers.setBearerAuth(apiToken);
+                    headers.set("Accept","application/json");
+                })
+                .retrieve()
+                .bodyToMono(String.class);
+    }
+
+    //Get a users list of rated TV shows.
+    public Mono<String> getUsersListOfRatedTv(String accountId){
+        return webClient.get()
+                .uri("/account/"+accountId+"/rated/tv")
+                .headers(headers->{
+                    headers.setBearerAuth(apiToken);
+                    headers.set("Accept","application/json");
+                })
+                .retrieve()
+                .bodyToMono(String.class);
+    }
+
+    //Get a users list of rated TV episodes.
+    public Mono<String> getListOfUserRatedTvEpisodes(String accountId){
+        return webClient.get()
+                .uri("/account/"+accountId+"/rated/tv/episodes")
+                .headers(headers->{
+                    headers.setBearerAuth(apiToken);
+                    headers.set("Accept","application/json");
+                })
+                .retrieve()
+                .bodyToMono(String.class);
+    }
+
+    //Get a list of movies added to a users watchlist.
+    public Mono<String> getListOfMoviesAddedToUsersWatchlist(String accountId){
+        return webClient.get()
+                .uri("/account/"+accountId+"/watchlist/movies")
+                .headers(headers->{
+                    headers.setBearerAuth(apiToken);
+                    headers.set("Accept","application/json");
+                })
+                .retrieve()
+                .bodyToMono(String.class);
+    }
+
+    //Get a list of TV shows added to a users watchlist.
+    public Mono<String> getListOfTvAddedToUsersWatchlist(String accountId){
+        return webClient.get()
+                .uri("/account/"+accountId+"/watchlist/tv")
+                .headers(headers->{
+                    headers.setBearerAuth(apiToken);
+                    headers.set("Accept","application/json");
+                })
+                .retrieve()
+                .bodyToMono(String.class);
+    }
+
+    //SECTION-ACCOUNT-END
+
+
+
+
 
 
     //Section-Discover
@@ -74,16 +230,7 @@ public class TmdbService {
                 .bodyToMono(MovieResponse.class);
     }
 
-    public Mono<String> getAccountDetails(String accountId) {
-        return webClient.get()
-                .uri("/account/" + accountId)
-                .headers(headers -> {
-                    headers.setBearerAuth(apiToken);  // Set Bearer token dynamically
-                    headers.set("Accept", "application/json");
-                })
-                .retrieve()
-                .bodyToMono(String.class);
-    }
+
 
     public Mono<String> getMovieByID(String movieId) {
         return webClient.get()
@@ -96,23 +243,13 @@ public class TmdbService {
                 .bodyToMono(String.class);
     }
 
-    public Mono<String> markAsFavorite(String accountId, int mediaId, String mediaType, boolean favorite) {
-        String url = "/account/" + accountId + "/favorite";
-
-        Map<String, Object> requestBody = Map.of(
-                "media_type", mediaType,  // "movie" or "tv"
-                "media_id", mediaId,
-                "favorite", favorite
-        );
-
-        return webClient.post()
-                .uri(url)
-                .headers(headers -> {
-                    headers.setBearerAuth(apiToken);  // Authentication
+    public Mono<String> getAvailableRegions(){
+        return webClient.get()
+                .uri("/watch/providers/regions")
+                .headers(headers->{
+                    headers.setBearerAuth(apiToken);
                     headers.set("Accept", "application/json");
-                    headers.set("Content-Type", "application/json");
                 })
-                .bodyValue(requestBody)  // Set JSON request body
                 .retrieve()
                 .bodyToMono(String.class);
     }
